6b7904d3c8ca36dc04a6ccea26fb0bf6f9bb5150
yangxiaoge <yangxiaoge1021@163.com>
2026-04-28 15:42:05 +0800

    Update clashScript.py

--- scripts/clashScript.py
+++ scripts/clashScript.py
@@ -19,6 +19,8 @@ def download_clash():
     # 创建文件夹
     if not os.path.exists("clash"):
         os.mkdir("clash")
+    if not os.path.exists("gkd"):
+        os.mkdir("gkd")
 
     def httpGetText(url):
         try:
